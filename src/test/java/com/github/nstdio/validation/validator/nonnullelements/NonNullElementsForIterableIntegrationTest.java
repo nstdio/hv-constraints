52bb4d594763c954791f48f3c4429a2b2f7c7a87
Edgar Asatryan <nstdio@gmail.com>
2018-12-29 16:51:50 +0400

    Add license header. Update pom.xml.

--- src/test/java/com/github/nstdio/validation/validator/nonnullelements/NonNullElementsForIterableIntegrationTest.java
+++ src/test/java/com/github/nstdio/validation/validator/nonnullelements/NonNullElementsForIterableIntegrationTest.java
@@ -1,18 +1,36 @@
+/*
+ * Copyright 2018 Edgar Asatryan <nstdio@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.github.nstdio.validation.validator.nonnullelements;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.validation.ConstraintViolation;
+
+import org.junit.jupiter.api.Test;
 
 import com.github.nstdio.validation.ValidatorIntegrationTest;
 import com.github.nstdio.validation.constraint.NonNullElements;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
+
 import lombok.Value;
-import org.junit.jupiter.api.Test;
-
-import javax.validation.ConstraintViolation;
-import java.util.Arrays;
-import java.util.Collection;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Edgar Asatryan
